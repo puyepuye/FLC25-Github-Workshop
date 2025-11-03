6b246afe1e812ed6537ae4436dc576adc608c0d4
bearatron <84403830+bearatron@users.noreply.github.com>
2025-11-03 14:35:44 -0500

    add profile pictures to each card

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 async function main() {
-  const grid = document.getElementById('grid');
-  const res = await fetch('data/contributors.json');
+  const grid = document.getElementById("grid");
+  const res = await fetch("data/contributors.json");
   const people = await res.json();
   people.sort((a, b) => a.name.localeCompare(b.name));
-  grid.innerHTML = people.map(p => `
+  grid.innerHTML = people
+    .map(
+      (p) => `
     <article class="card">
+      <img class="pfp" src="https://github.com/${p.github}.png?size=100">
+
       <h3>${p.name}</h3>
       <div class="handle">@${p.github}</div>
-      ${p.bio ? `<p>${p.bio}</p>` : ''}
-      <p><a href="https://github.com/${p.github}" target="_blank">GitHub Profile</a></p>
+      ${p.bio ? `<p>${p.bio}</p>` : ""}
+      <p><a href="https://github.com/${
+        p.github
+      }" target="_blank">GitHub Profile</a></p>
     </article>
-  `).join('');
+  `
+    )
+    .join("");
 }
 main().catch(console.error);
